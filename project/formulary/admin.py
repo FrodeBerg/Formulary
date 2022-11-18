a978e9f07a6c530b51ff8ddf48ff00e16a5552c5
FrodeBerg <bergstedt.frode@gmail.com>
2022-11-18 17:51:26 +0100

    added models

--- project/formulary/admin.py
+++ project/formulary/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+admin.site.register(Formula_Math)
+admin.site.register(Variable_Math)
+admin.site.register(Category_Math)
+
+admin.site.register(Formula_Phy)
+admin.site.register(Variable_Phy)
+admin.site.register(Category_Phy)
--- project/formulary/views.py
+++ project/formulary/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 from django.http import HttpResponse
 
 
 def index(request):
-    return render(request, 'formulary/index.html')
+    category_math = Category_Math.objects.all()
+    category_phy = Category_Phy.objects.all()
+
+    return render(request, 'formulary/index.html', {
+        "maths" : category_math,
+        "physics": category_phy
+    })
