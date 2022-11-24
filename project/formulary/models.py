9845eba6a7f340d0eb8a2cda8378178d38d30883
FrodeBerg <bergstedt.frode@gmail.com>
2022-11-24 19:37:09 +0100

    Rework 2.0

--- project/formulary/models.py
+++ project/formulary/models.py
@@ -2,6 +2,19 @@ from django.db import models
 
 # Create your models here.
 
+# Variables
+class Variable(models.Model):
+    variable = models.CharField(max_length=5)
+    description = models.CharField(max_length=50)
+
+# Formulas
+class Formula(models.Model):
+    formula = models.CharField(max_length=20)
+    using = models.ManyToManyField(Variable, related_name="using")
+    product = models.ManyToManyField(Variable, related_name="product")
+    math = models.BooleanField()
+
+
 
 
 
